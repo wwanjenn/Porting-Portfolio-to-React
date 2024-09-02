3421046b88fe01c3afcdb3cda65f7db3d78ff8cd
Lemonade7 <wmaglangit1118@gmail.com>
2024-09-02 21:35:40 +0800

    ?finished nav bar, tentative for bg redesign

--- my-react-portfolio/src/Components/Navbar/Navbar.jsx
+++ my-react-portfolio/src/Components/Navbar/Navbar.jsx
@@ -2,9 +2,16 @@ import React from "react"
 import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <header className="header">
+        <a href = "/" className ="Logo">Wanjin</a>
+        <nav className="navbar">
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Skills</a>
+            <a href="/">Portfolio</a>
+            <a href="/">Contact</a>
+        </nav>
+    </header>
   )
 };
 
